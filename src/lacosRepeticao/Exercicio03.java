fb7f50417a0173edd9ab63916b3db93af52fb8d2
Joice-Simao <joice.simao@hotmail.com>
2022-06-25 20:54:05 -0300

    Resolução exercício 03

---
+++ src/lacosRepeticao/Exercicio03.java
@@ -0,0 +1,19 @@
+/*Fácil 3 –    Faça um programa que verifique e mostre os números entre 1.000 e 2.000 (inclusive) que, 
+ * quando divididos por 11 produzam resto igual a 2. */
+
+package lacosRepeticao;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+
+		int resto = 0;
+
+		for (int numero = 1000; numero <= 2000; numero++) {
+			resto = numero % 11;
+			if (resto == 2) {
+				System.out.println("Número : " + numero + "  Resto : " + resto);
+			}
+		}
+	}
+}
