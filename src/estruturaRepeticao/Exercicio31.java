351211b9e806e38800cb767288c24d5350940593
Joice-Simao <joice.simao@hotmail.com>
2023-02-02 17:40:55 -0300

    Resolução exercício 31

--- src/estruturaRepeticao/Exercicio31.java
+++ src/estruturaRepeticao/Exercicio31.java
@@ -4,5 +4,27 @@
  */
 package estruturaRepeticao;
 
+import java.util.Scanner;
+
 public class Exercicio31 {
+
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int idade, contador = 0, totalIdade = 0, mediaIdades;
+
+        do {
+         System.out.println("Informe a idade : ");
+         idade = scanner.nextInt();
+            totalIdade = idade + totalIdade;
+            contador++;
+
+        } while (idade != 0);
+
+        contador = contador - 1;
+        mediaIdades = totalIdade / contador;
+
+        System.out.println("Média das idades digitadas : " +mediaIdades);
+
+    }
 }
