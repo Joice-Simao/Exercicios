615b3a88df072f9dee6cfc03e132ff410db76a09
Joice-Simao <joice.simao@hotmail.com>
2023-02-01 23:22:42 -0300

    Exercício 31

---
+++ src/estruturaRepeticao/Exercicio31.java
@@ -0,0 +1,8 @@
+/*
+    Intermediário 31- Faça um programa que receba várias idades e que calcule e mostre a média das idades digitadas.
+     Finalize digitando idade igual a zero.
+ */
+package estruturaRepeticao;
+
+public class Exercicio31 {
+}
