e99182859afd2afbba19caccbf73501d2178719e
Joice-Simao <joice.simao@hotmail.com>
2023-02-10 23:28:00 -0300

    Exercpicio 41

---
+++ src/estruturaRepeticao/Exercicio41.java
@@ -0,0 +1,10 @@
+/*
+    Intermediário 41-  Faça um programa que receba a idade e a altura de várias pessoas e que calcule
+    e mostre a média das alturas das pessoas com mais de 50 anos. Para encerrar a entrada de dados
+    digite idade menor ou igual a zero.
+ */
+package estruturaRepeticao;
+
+public class Exercicio41 {
+
+}
