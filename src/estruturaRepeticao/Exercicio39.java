3afa4a0ba815a7c75f14affcd28e71f0b373ff58
Joice-Simao <joice.simao@hotmail.com>
2023-02-10 23:15:36 -0300

    Resolução exercício 39

--- src/estruturaRepeticao/Exercicio39.java
+++ src/estruturaRepeticao/Exercicio39.java
@@ -1,10 +1,10 @@
 /*
-    Intermedi·rio 39- Em uma eleiÁ„o presidencial existem quatro candidatos. Os votos s„o informados
-    por meio de cÛdigo. Os cÛdigos utilizados s„o:
+    Intermediario 39- Em uma elei√ßao presidencial existem quatro candidatos. Os votos sao informados
+    por meio de codigo. Os codigos utilizados sao:
         1, 2, 3, 4	Votos para os respectivos candidatos
         5	Voto nulo
         6	Voto em branco
-    FaÁa um programa que calcule e mostre:
+    Fa√ßa um programa que calcule e mostre:
             O total de votos para cada candidato;
             O total de votos nulos:
             O total de votos em branco;
@@ -14,5 +14,56 @@
  */
 package estruturaRepeticao;
 
+import java.util.Scanner;
+
 public class Exercicio39 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int voto, totalJoao = 0, totalSarah = 0, totalPaulo = 0, totalEster = 0, totalNulo =0, totalBranco = 0,
+                totalVotos = 0, percentualNulos, percentualBrancos;
+
+        do {
+            System.out.println("Sistema de vota√ß√£o\n 1 - Joao\n 2 - Sarah\n 3 - Paulo\n 4 - Ester" +
+                    "\n Digite o n√∫mero do seu candidato: ");
+            voto = scanner.nextInt();
+            totalVotos++;
+
+            switch (voto){
+                case 1:
+                    totalJoao++;
+                    break;
+                case 2:
+                    totalSarah++;
+                    break;
+                case 3:
+                    totalPaulo++;
+                    break;
+                case 4:
+                    totalEster++;
+                    break;
+                case 5:
+                    totalNulo++;
+                    break;
+                case 6:
+                    totalBranco++;
+                    break;
+
+            }
+        }while (voto != 0);
+
+        percentualNulos = (totalNulo * 100) / totalVotos;
+        percentualBrancos = (totalBranco * 100) / totalVotos;
+
+        System.out.println("Resultado da vota√ß√£o"+
+                "\nJoao: " +totalJoao+
+                "\nSarah: " +totalSarah+
+                "\nPaulo: " +totalPaulo+
+                "\nEster: " +totalEster+
+                "\nVotos nulos: " +totalNulo+
+                "\nVotos em branco: " +totalBranco+
+                "\nPercentual votos nulos: " +percentualNulos+ "%"+
+                "\nPercentual votos brancos: " +percentualBrancos+ "%");
+
+    }
 }
