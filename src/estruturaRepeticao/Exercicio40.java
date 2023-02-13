356c6cf20fd51c5f75a71fd4c93ecc0a40d6d5c9
Joice-Simao <joice.simao@hotmail.com>
2023-02-13 16:22:17 -0300

    Exercício 40

---
+++ src/estruturaRepeticao/Exercicio40.java
@@ -0,0 +1,9 @@
+/*
+    Intermediário 40- Faça um programa que receba como entrada uma lista de números positivos ou negativos, terminada
+    com o número zero. O programa deve usar laços de repetição para produzir como saída a soma dos números positivos,
+    a soma dos números negativos e a soma das duas somas parciais.
+ */
+package estruturaRepeticao;
+
+public class Exercicio40 {
+}
