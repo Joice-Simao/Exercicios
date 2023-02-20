29c9963f7e3bfa92aee84958af8bfd64f2e0a4a8
Joice-Simao <joice.simao@hotmail.com>
2023-02-19 23:46:42 -0300

    Exercicio 02

--- src/vetores/Exercicio01.java
+++ src/vetores/Exercicio01.java
@@ -6,8 +6,27 @@
 
 package vetores;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 12;
+        int v[] = new int[n];
+        int i, x, y;
+
+        for (i = 0; i < n; i++){
+            System.out.printf("Informe %2d° valor de %d: ", (i+1),n);
+            v[i] = scanner.nextInt();
+        }
+
+        System.out.print("Posição x: ");
+        x = scanner.nextInt();
+
+        System.out.print("Posição y: ");
+        y = scanner.nextInt();
+
+
     }
 }
