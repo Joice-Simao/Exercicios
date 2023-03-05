d4b20b29418e142a0043ed4a0690a54f6fce8e5b
Joice-Simao <joice.simao@hotmail.com>
2023-03-04 23:32:09 -0300

    Resolução exercício 01

--- src/vetores/Exercicio01.java
+++ src/vetores/Exercicio01.java
@@ -1,7 +1,7 @@
 /*
-    Fácil 1. Leia um vetor de 12 posições e em seguida ler também dois valores X e Y quaisquer correspondentes
-    a duas posições no vetor. Ao final seu programa deverá escrever a soma dos valores encontrados nas
-    respectivas posições X e Y.
+    Facil 1. Leia um vetor de 12 posicoes e em seguida ler tambem dois valores X e Y quaisquer correspondentes
+    a duas posicoes no vetor. Ao final seu programa devera escrever a soma dos valores encontrados nas
+    respectivas posicoes X e Y.
  */
 
 package vetores;
@@ -9,24 +9,25 @@ package vetores;
 import java.util.Scanner;
 
 public class Exercicio01 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = 12;
+        int n = 4;
         int v[] = new int[n];
-        int i, x, y;
+        int i, x, y, resultadoSoma;
 
-        for (i = 0; i < n; i++){
-            System.out.printf("Informe %2d° valor de %d: ", (i+1),n);
+        for (i = 0; i < 4; i++) {
+            System.out.printf("Informe%2d valor de %d: ", (i + 1), n);
             v[i] = scanner.nextInt();
         }
+        System.out.print("Posicao de X: ");
+        x = scanner.nextInt() - 1;
 
-        System.out.print("Posição x: ");
-        x = scanner.nextInt();
+        System.out.print("Posicao de X: ");
+        y = scanner.nextInt() - 1;
 
-        System.out.print("Posição y: ");
-        y = scanner.nextInt();
+        resultadoSoma = v[x] + v[y];
 
-
+        System.out.print("Soma dos valores nas posicoes X e Y: " +v[x]+ " + " +v[y] +" = " +resultadoSoma);
     }
 }
