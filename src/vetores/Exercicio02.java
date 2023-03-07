65e2a4bb91e39af1b5b7bd6d24f806965b97e9a0
Joice-Simao <joice.simao@hotmail.com>
2023-03-06 23:13:24 -0300

    Ajustes

--- src/vetores/Exercicio02.java
+++ src/vetores/Exercicio02.java
@@ -9,7 +9,7 @@ public class Exercicio02 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        int n = 3, i, numero;
+        int n = 10, i, numero;
         int v[] = new int[n];
 
         for(i = 0; i < n; i++){
@@ -17,7 +17,7 @@ public class Exercicio02 {
                 System.out.print("\nDigite um número: ");
                 numero = scanner.nextInt();
 
-            }while((numero%2)!= 1);
+            }while((numero % 2)!= 1);
             v[i]=numero;
         }
 
