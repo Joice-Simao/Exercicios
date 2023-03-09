ae6e882091b73ae55b9a36bccf70c9183e99313a
Joice-Simao <joice.simao@hotmail.com>
2023-03-08 23:24:09 -0300

    Exercicio 04

--- src/vetores/Exercicio04.java
+++ src/vetores/Exercicio04.java
@@ -4,5 +4,30 @@
  */
 package vetores;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 4, i, x;
+        int v[] = new int[n];
+
+        for (i = 0; i < n; i++){
+            System.out.print("Digite um número para posição " +(i+1)+ ": ");
+            v[i] = scanner.nextInt();
+        }
+
+        System.out.print("Digite valor de X: ");
+        x = scanner.nextInt();
+
+
+        for (i = 0; i < n; i++){
+            if(v[i] == x) {
+                System.out.println("O índice do número digitado é " + i);
+            }
+        }
+
+
+    }
 }
