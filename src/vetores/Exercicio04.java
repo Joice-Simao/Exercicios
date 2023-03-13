cdea2511719632e8e6d37094a806d3408579884f
Joice-Simao <joice.simao@hotmail.com>
2023-03-13 11:26:25 -0300

    Correção do exercício 04

--- src/vetores/Exercicio04.java
+++ src/vetores/Exercicio04.java
@@ -10,24 +10,31 @@ public class Exercicio04 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        int n = 4, i, x;
+        int n = 4, i, x, posicao = 0;
         int v[] = new int[n];
 
+        // entrada de dados do vetor
         for (i = 0; i < n; i++){
             System.out.print("Digite um número para posição " +(i+1)+ ": ");
             v[i] = scanner.nextInt();
         }
 
+        // valor de X
         System.out.print("Digite valor de X: ");
         x = scanner.nextInt();
 
+        // buscando valor de X no vetor
         for (i = 0; i < n; i++){
-
             if(v[i] == x) {
-                System.out.println("A posição do número digitado é " + (i+1));
-            }else
-                System.out.println("Valor não encontrado");
-                break;
+                posicao = i + 1;
+            }
         }
+
+        // saída de dados
+        if(posicao > 0) {
+            System.out.println("Valor " +x+ " encontrado na posição " +posicao);
+        }else
+            System.out.println("Valor não encontrado no vetor");
+
     }
 }
