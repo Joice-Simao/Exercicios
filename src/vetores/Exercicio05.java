43779aae6446b194ee2df5becf9f0779fb29fb59
Joice-Simao <joice.simao@hotmail.com>
2023-03-14 20:49:11 -0300

    Resolução exercício 05

--- src/vetores/Exercicio05.java
+++ src/vetores/Exercicio05.java
@@ -3,5 +3,27 @@
  */
 package vetores;
 
+import java.util.Scanner;
+
 public class Exercicio05 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 40, i, contador = 0;
+        int vetor[] = new int [n];
+
+        for (i = 0; i < n; i++){
+            System.out.print("Digite um número para posição " +(i+1)+ ": ");
+            vetor[i] = scanner.nextInt();
+        }
+
+        for(i = 0; i < n; i++){
+            if (vetor[i] % 2 == 0){
+                contador++;
+            }
+        }
+
+        System.out.print("\nQuantidade de valores pares no vetor: " +contador);
+
+    }
 }
